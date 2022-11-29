93313aa826ec0ea714875d1dd4774b2846ae8352
MariamMEK <65685526+mariaammekarishvili@users.noreply.github.com>
2022-11-29 16:02:57 +0400

    export as object

--- stuff.js
+++ stuff.js
@@ -2,10 +2,16 @@ const counter = function (arr) {
   return "there are " + arr.length + " elements in this array";
 };
 
-module.exports.adder = function (a, b) {
+const adder = function (a, b) {
   return `the sum of 2 number is ${a + b}`;
 };
 
-module.exports.pi = 3.142;
+const pi = 3.142
 
-module.exports.counter = counter;
+module.exports = {
+    adder: adder,
+    counter: counter,
+    pi: pi,
+};
+
+
